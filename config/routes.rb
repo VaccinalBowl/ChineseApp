57d7f6aeb66f1153bf6619f0fd11b13c557088c4
VaccinalBowl <jcummin@tcd.ie>
2014-12-22 23:16:08 +0000

    static

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#home'
+  #root 'application#home'
   # Example of regular route:
   #get 'words/addmany' 
   #get 'words/destroyall' => 'words#destroyall'

308105f5a0d8da45e44db62322590badfebc5da0
VaccinalBowl <jcummin@tcd.ie>
2014-12-23 22:38:24 +0000

    Finish login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,9 +44,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-   test "password should have a minimum length" do
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
-   end
-   
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
